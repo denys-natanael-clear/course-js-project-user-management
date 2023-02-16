2a7b4515c875be322b50edd2547305b5e5e80ce5
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-16 10:37:17 -0300

    feat: lessons 33, 34 and 35: applying the forEach functionality, validating the forEach with if and else and adding the information in a json.

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ var email = document.querySelector('#exampleInputEmail')
 var password = document.querySelector('#exampleInputPassword')
 var photo = document.querySelector('#exampleInputFile')
 var admin = document.querySelector('#exampleInputAdmin')
+
+
+var fields = document.querySelectorAll('#form-user-create [name]')
+var user = {}
+
+fields.forEach(function(field, index){
+
+    if (field.name == 'gender') {
+
+        if (field.checked) {
+
+            user[field.name] = field.value
+
+        }
+    } else {
+
+        user[field.name] = field.value
+
+    }
+
+})
+
+console.log(user)
